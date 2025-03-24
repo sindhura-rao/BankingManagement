6a80f81f1cddb383e5b5c882ad8a7b3f0ce0754c
Soma Potta <somasekhar.potta@gmail.com>
2025-03-24 13:03:29 +1100

    added banktranction classes

---
+++ BankingManagement/BankAccount.cs
@@ -0,0 +1,75 @@
+﻿namespace Classes;
+
+public class BankAccount
+{
+    private static int s_accountNumberSeed = 1234567890;
+    private List<Transaction> _allTransactions = new List<Transaction>();
+    public static DateTime Now { get; }
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
+
+        Owner = name;
+        MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+    }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        var deposit = new Transaction(amount, date, note);
+        _allTransactions.Add(deposit);
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
+    }
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in _allTransactions)
+            {
+                balance = balance + item.Amount;
+                //balance += item.Amount; same as above
+            }
+
+            return balance;
+        }
+    }
+    
+
+        
+    
+        
+}
+
+
+
+
+    
+
+
+
+
+
+    
+
---
+++ BankingManagement/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Classes;
+var account = new BankAccount("<sindhu>", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+var account2 = new BankAccount("<somu>", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+Console.WriteLine($"Sindhu balance {account.Balance}");
+Console.WriteLine($"somu balance {account2.Balance}");
+
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine($"Sindhu {account.Balance}");
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+BankAccount invalidAccount;
+
+//Test that the initial balances must be positive.
+try
+{
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    return;
+}
+
+//Test for a negative balance.
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
+
+
+
+
+
